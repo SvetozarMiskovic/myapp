184b304119a434e300574c7a7e581baecfbae5e2
SvetozarMiskovic <svetomiskovic98@gmail.com>
2021-12-22 18:55:35 +0100

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../App.css';
+import FormBuilder from '../domains/demo/FormBuilderApp/FormBuilder';
+
+function App() {
+  return (
+    <div className="App">
+      <FormBuilder />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/domains/demo/FormBuilderApp/FormBuilder.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function FormBuilder() {
+  return (
+    <div>
+      <h1>Some dummy text</h1>
+    </div>
+  );
+}
+
+export default FormBuilder;
